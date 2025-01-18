3507df95b3000b816aa506e52c30610d95f5dc19
Juan Pablo <juanpablo.canepa21@gmail.com>
2025-01-18 18:22:31 -0500

    chore: fisrt commit

---
+++ src/main/java/com/jp21/apache/kafka/KafkaProducerApplication.java
@@ -0,0 +1,42 @@
+package com.jp21.apache.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class KafkaProducerApplication {
+
+    private static Logger log = LoggerFactory.getLogger(KafkaProducerApplication.class);
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("acks", "1"); // 0, 1, all [no ack, leader ack, all ack]
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("linger.ms", "15");
+
+        try (Producer<String, String> producer = new KafkaProducer<>(properties);) {
+
+            for (int i = 0; i < 1_000_000; i++) {
+                log.info("Sending message: " + i);
+                producer.send(new ProducerRecord<>("my-topic", Integer.toString(i), Integer.toString(i)));
+            }
+            producer.flush();
+        }
+
+        log.info("Time taken: " + (System.currentTimeMillis() - startTime) + " ms");
+        // 4994 ms
+        // 3604 ms | linger 6
+        // 3711 ms | linger 4
+        // 3611 ms | linger 10
+        // 3356 ms | linger 15
+
+    }
+}
